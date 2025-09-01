7b8d073d6a4d40137523504cb593a71015ac4815
isaquepamplona <isaquepamplonaa@gmail.com>
2025-09-01 19:23:13 -0300

    adição e subtração
    Adição e subtração

--- Program.cs
+++ Program.cs
@@ -3,6 +3,28 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // adição - Pamplo
+    float a;
+    float b;
+
+    Console.WriteLine("primeiro número: ");
+    a = float.Parse(Console.ReadLine());
+    
+    Console.WriteLine("segundo número: ");
+    b = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("resultado  " + (a + b));
+
+
+   // multiplicação - Pamplo
+    float c;
+    float d;
+    
+    Console.WriteLine("primeiro número: ");
+    c = float.Parse(Console.ReadLine());
+    Console.WriteLine("segundo número: ");
+    d = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("resultado  " + (c * d));
     }
 }
