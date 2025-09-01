d9bfc4ecb67b71d73e7447e09276a0ae27c16adb
dracowen <wendylyvi@gmail.com>
2025-09-01 19:33:12 -0300

    subtração e divisão

--- Program.cs
+++ Program.cs
@@ -3,6 +3,29 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+       // subtração - Wendy 
+    float a;
+    float b;
+
+    Console.WriteLine("primeiro número: ");
+    a = float.Parse(Console.ReadLine());
+    
+    Console.WriteLine("segundo número: ");
+    b = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("resultado  " + (a - b));
+
+
+   // Divisão - Wendy 
+    float c;
+    float d;
+    
+    Console.WriteLine("primeiro número: ");
+    c = float.Parse(Console.ReadLine());
+    Console.WriteLine("segundo número: ");
+    d = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("resultado  " + (c / d));
+
     }
 }
